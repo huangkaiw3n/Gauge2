efecf8c4386b3e42cab7df3e71bb1555882b5d11
Kaiwen <huangkaiw3n@gmail.com>
2015-03-19 02:19:17 +0800

    User.java to hold user's fields: username, password, email, ip, etc.

---
+++ core/src/main/java/org/gauge/User.java
@@ -0,0 +1,62 @@
+package org.gauge;
+
+/**
+ * Created by Kaiwen on 19/3/2015.
+ */
+public class User {
+    private String username, password, email, ip;
+
+    public User(String username) {
+        this.username = username;
+    }
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public User(String username, String password, String email) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
+
+    public User(String username, String password, String email, String ip) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.ip = ip;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setIp(String ip) {
+        this.ip = ip;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getIp() {
+        return ip;
+    }
+}
