3b722ac7a47da79603f91428e61cbf9bc4468dd9
lim lim <zoids911@hotmail.com>
2015-04-08 17:28:18 +0800

    Reformatted CLient layout and implemented Leave Chatroom (tested) Chatrooms and ActiveUsers now display

--- client/src/main/java/org/gauge/ui/MainView.java
+++ client/src/main/java/org/gauge/ui/MainView.java
@@ -1,20 +1,13 @@
 package org.gauge.ui;
 
-import com.intellij.uiDesigner.core.GridLayoutManager;
 import org.gauge.*;
-import sun.applet.Main;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import javax.swing.*;
-import javax.swing.event.ListDataListener;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
-import java.awt.*;
 import java.awt.event.*;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Set;
-import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * Created by joel on 3/14/15.
@@ -22,16 +15,16 @@ import java.util.concurrent.ConcurrentHashMap;
 public class MainView extends JPanel {
     private JPanel panel1;
 
-    private JTextField DisplayMessage;
+    private JTextArea DisplayMessage;
 
-    private JTextArea MessageByUser;
+    private JTextField MessageByUser;
 
     private JList Rooms;
     private JList ActiveUsers;
     private JList ChatRoomJoined;
 
     private JButton leaveButton;
-    private JButton Leave;
+    private JButton JoinButton;
     private JButton Join;
     private JButton Create;
     private JButton Refresh;
@@ -52,6 +45,7 @@ public class MainView extends JPanel {
     private Object [] RoomsAvailable;
 
     public MainView(final User user1) {
+
         try{
             App.client.loadUserlist();
             Thread.sleep(400);
@@ -76,6 +70,7 @@ public class MainView extends JPanel {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
+
 
 
         Refresh.addActionListener(new ActionListener() {
@@ -117,25 +112,27 @@ public class MainView extends JPanel {
             }
         });
 
-        Leave.addActionListener(new ActionListener() {
+        JoinButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 //Leaves a chat room the user is in
-                App.client.leave(chatRoomId);
+
             }
         });
 
         SendButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 //Sends whatever message that is in MessageByUser
+                StringBuilder textInDM = new StringBuilder();
                 String input = MessageByUser.getText();
                 try{
                     App.client.message(chatRoomId, input);
                     MessageByUser.setText("");
+                    textInDM.append(DisplayMessage.getText() + "\n");
+                    textInDM.append(user1.getUsername() + ": \n" + input + "\n");
+                    DisplayMessage.setText(textInDM.toString());
                 }catch(Exception e2){
                     DisplayMessage.setText("Please select user/chat room first");
                 }
-
-
             }
         });
 
@@ -152,7 +149,32 @@ public class MainView extends JPanel {
         });
 
         ChatRoomJoined.addListSelectionListener(new ListSelectionListener() {
-            public void valueChanged(ListSelectionEvent e) {chatRoomId = Rooms.getSelectedValue().toString();
+            public void valueChanged(ListSelectionEvent e) {
+                StringBuilder sb = new StringBuilder();
+                String payload, username, message;
+                JSONObject jsonMessage, jsonUser;
+                chatRoomId = ChatRoomJoined.getSelectedValue().toString();
+                DisplayMessage.setText("");
+                while(App.client.getInbox(chatRoomId).size() > 0){
+                    payload = App.client.getInbox(chatRoomId).poll().getPayload();
+                    message = username = null;
+                    try{
+                        jsonMessage = new JSONObject(payload);
+                        jsonUser = new JSONObject(jsonMessage.get("user"));
+                        message = jsonMessage.get("body").toString();
+                        username = jsonUser.get("username").toString();
+                        if(message != null && username != null) {
+                            sb.append(username);
+                            sb.append(":\r\n");
+                            sb.append(message);
+                            sb.append("\r\n\r\n");
+                        }
+                    }catch(JSONException e3){
+                        e3.printStackTrace();
+                    }
+
+                }
+                DisplayMessage.setText(sb.toString());
             }
         });
 
@@ -161,5 +183,32 @@ public class MainView extends JPanel {
                 //App.client.
             }
         });
+        leaveButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                try{
+                    App.client.leave(chatRoomId);
+                }catch(Exception e5){
+                    DisplayMessage.setText("No chat room selected");
+                }
+
+            }
+        });
+
+        MessageByUser.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
+                    StringBuilder textInDM = new StringBuilder();
+                    String input = MessageByUser.getText();
+                    try{
+                        App.client.message(chatRoomId, input);
+                        MessageByUser.setText("");
+                        textInDM.append(DisplayMessage.getText() + "\n");
+                        textInDM.append(user1.getUsername() + ": \n" + input + "\n");
+                        DisplayMessage.setText(textInDM.toString());
+                    }catch(Exception e2){
+                        DisplayMessage.setText("Please select user/chat room first");
+                    }
+                }
+        });
+
     }
 }
