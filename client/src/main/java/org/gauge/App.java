fe2e0586d7ae1ca3d6690c133a38df952b049953
Huang Kaiwen <huangkaiw3n@gmail.com>
2015-04-08 12:29:59 +0800

    Modified Client App to take in server address in gui before starting client thread.

--- client/src/main/java/org/gauge/App.java
+++ client/src/main/java/org/gauge/App.java
@@ -23,18 +23,10 @@ public class App {
 
 
     public static void main(final String[] args) {
-        log.info("Starting client..");
-        client = new Client("127.0.0.1", 9000, 9060);
-        client.start();
 
-        // GUI
-      /*
-    Runnable runnableGui = new Runnable() {
-      public void run() {
-        MainView.runForm(args);
-      }
-    };
-    */
+        //client = new Client("127.0.0.1", 9000, 9060);
+        //client.start();
+
         Runnable runnableGui = new Runnable() {
             public void run() {
                 Login lg = new Login();
