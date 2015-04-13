b9bd3b3ea4df6b7d349a38c1cf91d7747471acf0
Joel Tong <me@joeltong.org>
2015-04-13 17:44:26 +0800

    fixed bug

--- server/src/main/java/org/gauge/resources/Registration.java
+++ server/src/main/java/org/gauge/resources/Registration.java
@@ -4,36 +4,70 @@ package org.gauge.resources;
  * Created by Kaiwen on 12/4/2015.
  */
 public class Registration {
-    public final static String registration =
-            "    <!DOCTYPE html>\n" +
-            "    <html>\n" +
-            "    <head>\n" +
+    public final static String registration = "<!DOCTYPE html>\n" +
+            "<html>\n" +
+            "<head>\n" +
             "    <title>Test submit form</title>\n" +
             "    <!--<script language=\"JavaScript\" type=\"text/javascript\" src=\"js/voucher.js\"></script>-->\n" +
             "    <script src=\"https://code.jquery.com/jquery-1.11.2.min.js\"></script>\n" +
+            "    <link rel='stylesheet' href='//cdnjs.cloudflare.com/ajax/libs/semantic-ui/1.1.2/semantic.min.css'/>\n" +
             "    <meta charset=\"utf-8\" />\n" +
-            "    </head>\n" +
-            "    <body>\n" +
-            "    <p style=\"font-size:30px\">Gauge Client Registration</p>\n" +
-            "    Registered Users: <span id=\"totalUsers\">0</span>\n" +
-            "    Online Users: <span id=\"onlineUsers\">0</span>\n" +
-            "    <form id=\"loginForm\" name=\"form\" action=\"register\">\n" +
-            "    <div class=\"form-item\">\n" +
-            "    <label>Username:</label>\n" +
-            "    <input type=\"text\" id=\"username\" name=\"username\" maxlength = \"30\"/>\n" +
+            "    <style>\n" +
+            "        .container {\n" +
+            "            margin: auto;\n" +
+            "            width: 100%;\n" +
+            "            height: 100%;\n" +
+            "            background-color: red;\n" +
+            "            background-image: url(\"http://cdn.backgroundhost.com/backgrounds/subtlepatterns/batthern.png\")\n" +
+            "        }\n" +
+            "\n" +
+            "        .container-inner {\n" +
+            "            margin: auto;\n" +
+            "            position: relative;\n" +
+            "            right: 0;\n" +
+            "            display: table;\n" +
+            "        }\n" +
+            "    </style>\n" +
+            "</head>\n" +
+            "<body>\n" +
+            "<div class=\"container\">\n" +
+            "    <div class=\"container-inner\">\n" +
+            "        <div class=\"ui hidden divider\"></div>\n" +
+            "        <div class=\"ui center aligned segment card\">\n" +
+            "            <p style=\"font-size:30px\">Gauge Client Registration</p>\n" +
+            "            <div class=\"ui statistic\">\n" +
+            "                <div class=\"value\"><span id\"=totalUsers\">0</span></div>\n" +
+            "                <div class=\"label\">Registered</div>\n" +
+            "            </div>\n" +
+            "            <div class=\"ui statistic\">\n" +
+            "                <div class=\"value\"><span id\"=onlineUsers\">0</span></div>\n" +
+            "                <div class=\"label\">Online</div>\n" +
+            "            </div>\n" +
+            "            <form id=\"loginForm\" name=\"form\" action=\"register\">\n" +
+            "                <div class=\"form-item ui labeled input\">\n" +
+            "                    <label class=\"ui label\">Username:</label>\n" +
+            "                    <input type=\"text\" placeholder=\"username\" id=\"username\" name=\"username\" maxlength = \"30\"/>\n" +
+            "                </div>\n" +
+            "                <div class=\"ui hidden divider\"></div>\n" +
+            "                <div class=\"form-item ui labeled input\">\n" +
+            "                    <label class=\"ui label\">Password: </label>\n" +
+            "                    <input id=\"password\"  placeholder=\"password\" name=\"password\" type=\"password\" maxlength = \"30\"/>\n" +
+            "                </div>\n" +
+            "                <div class=\"ui hidden divider\"></div>\n" +
+            "                <div class=\"form-item ui labeled input\">\n" +
+            "                    <label class=\"ui label\">Email:    </label>\n" +
+            "                    <input id=\"email\"  placeholder=\"email\" name=\"email\" type=\"text\" maxlength = \"30\"/>\n" +
+            "                </div>\n" +
+            "                <div class=\"ui hidden divider\"></div>\n" +
+            "                <input class=\"ui blue button\" type=\"submit\" name=\"submit\" value=\"Submit\" onClick='getData()'/>\n" +
+            "                <div>\n" +
+            "                </div>\n" +
+            "            </form>\n" +
+            "        </div>\n" +
             "    </div>\n" +
-            "    <div class=\"form-item\">\n" +
-            "    <label>Password: </label>\n" +
-            "    <input id=\"password\" name=\"password\" type=\"password\" maxlength = \"30\"/>\n" +
-            "    </div>\n" +
-            "    <div class=\"form-item\">\n" +
-            "    <label>Email:    </label>\n" +
-            "    <input id=\"email\" name=\"email\" type=\"text\" maxlength = \"30\"/>\n" +
-            "    </div>\n" +
-            "    <input type=\"submit\" name=\"submit\" value=\"Submit\" onClick='getData()'/>\n" +
-            "    </form>\n" +
-            "    </body>\n" +
-            "    <script>\n" +
+            "</div>\n" +
+            "</body>\n" +
+            "<script>\n" +
             "    $(document).ready(function () {\n" +
             "        document.getData = function () {\n" +
             "            var $params = $('#loginForm :input');\n" +
@@ -51,19 +85,19 @@ public class Registration {
             "        document.numberUsers = 0;\n" +
             "        setInterval(function() {\n" +
             "            $.ajax({\n" +
-            "                    url: \"/userStats?\",\n" +
-            "                    type: \"GET\",\n" +
-            "                    crossDomain: true,\n" +
-            "                    data: null,\n" +
-            "                    dataType: \"json\",\n" +
-            "                    success: function(data) {\n" +
-            "                $('#totalUsers').html(data.totalUsers + \"\");\n" +
-            "                $('#onlineUsers').html(data.onlineUsers + \"\");\n" +
-            "            }\n" +
+            "                url: \"/userStats?\",\n" +
+            "                type: \"GET\",\n" +
+            "                crossDomain: true,\n" +
+            "                data: null,\n" +
+            "                dataType: \"json\",\n" +
+            "                success: function(data) {\n" +
+            "                    $('#totalUsers').html(data.totalUsers + \"\");\n" +
+            "                    $('#onlineUsers').html(data.onlineUsers + \"\");\n" +
+            "                }\n" +
             "            });\n" +
             "        }, 1000);\n" +
             "    });\n" +
-            "    </script>\n" +
-            "    </html>";
+            "</script>\n" +
+            "</html>";
 
 }
